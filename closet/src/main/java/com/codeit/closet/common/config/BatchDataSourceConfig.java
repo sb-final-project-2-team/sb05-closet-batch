7f30a0be9c88fd9fe5fd48a9aac6370bcef9f353
Namsoo315 <gustn2130@naver.com>
2026-01-15 09:16:20 +0900

    feat: Batch 사용용 기본 엔티티 추가 및 BatchConfig 추가

---
+++ closet/src/main/java/com/codeit/closet/common/config/BatchDataSourceConfig.java
@@ -0,0 +1,53 @@
+package com.codeit.closet.common.config;
+
+import jakarta.persistence.EntityManagerFactory;
+import javax.sql.DataSource;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.boot.orm.jpa.EntityManagerFactoryBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+@Configuration
+@EnableTransactionManagement
+@EnableJpaRepositories(
+    basePackages = "com.codeit.closet",
+    entityManagerFactoryRef = "batchEntityManagerFactory",
+    transactionManagerRef = "batchTransactionManager"
+)
+public class BatchDataSourceConfig {
+
+  @Primary
+  @Bean(name = "batchDataSource")
+  @ConfigurationProperties(prefix = "spring.batch.datasource")
+  public DataSource batchDataSource() {
+    return DataSourceBuilder.create().build();
+  }
+
+  @Primary
+  @Bean(name = "batchEntityManagerFactory")
+  public LocalContainerEntityManagerFactoryBean batchEntityManagerFactory(
+      EntityManagerFactoryBuilder builder,
+      @Qualifier("batchDataSource") DataSource batchDataSource
+  ) {
+    return builder
+        .dataSource(batchDataSource)
+        .packages("com.codeit.closet")
+        .build();
+  }
+
+  @Primary
+  @Bean(name = "batchTransactionManager")
+  public PlatformTransactionManager batchTransactionManager(
+      final @Qualifier("batchEntityManagerFactory") EntityManagerFactory batchEntityManagerFactory
+  ) {
+    return new JpaTransactionManager(batchEntityManagerFactory);
+  }
+}
---
+++ closet/src/main/java/com/codeit/closet/common/config/BusinessDataSourceConfig.java
@@ -0,0 +1,47 @@
+package com.codeit.closet.common.config;
+
+import jakarta.persistence.EntityManagerFactory;
+import javax.sql.DataSource;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.boot.orm.jpa.EntityManagerFactoryBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+@Configuration
+@EnableTransactionManagement
+@EnableJpaRepositories(
+    basePackages = "com.codeit.closet",
+    entityManagerFactoryRef = "businessManagerFactory",
+    transactionManagerRef = ""
+)
+public class BusinessDataSourceConfig {
+
+  @Bean(name = "businessDataSource")
+  @ConfigurationProperties(prefix = "spring.business.datasource")
+  public DataSource businessDataSource() {
+    return DataSourceBuilder.create().build();
+  }
+
+  @Bean
+  public LocalContainerEntityManagerFactoryBean businessManagerFactory(
+      EntityManagerFactoryBuilder builder,
+      @Qualifier("businessDataSource") DataSource businessDataSource) {
+    return builder
+        .dataSource(businessDataSource)
+        .packages("com.codeit.closet")
+        .build();
+  }
+
+  @Bean
+  public PlatformTransactionManager businessTransactionManager(
+      @Qualifier("businessManagerFactory") EntityManagerFactory businessManagerFactory) {
+    return new JpaTransactionManager(businessManagerFactory);
+  }
+}
